e5aa0993a4be272e08d658bcea785f547c35c0cb
Line <anna.line2507@gmail.com>
2025-04-02 21:43:14 +0200

    added add modal with styling

--- OO-P/src/js/Ui.js
+++ OO-P/src/js/Ui.js
@@ -5,37 +5,33 @@ class Ui {
         this.renderData(id);
     }
 
-    // Open form when add button is clicked
-    static openFormOnClick = (form, id) => {
+    // Open the form when the add button is clicked
+    static openFormOnClick = () => {
         const addButton = document.querySelector(".button-add");
+        const modal = document.querySelector(".form-modal__all");
         const closeButton = document.querySelector(".form-modal__close");
 
-        // Open form when add button is clicked
+        // Open form when the add button is clicked
         addButton.addEventListener("click", () => {
-            Ui.openForm(form, id);
+            modal.classList.add("form-modal__open");  // Show the modal
         });
 
-        // Close the form when the close button is clicked.
+        // Close the modal when the close button is clicked
         closeButton.addEventListener("click", () => {
-            Ui.closeForm(form, id);
+            modal.classList.remove("form-modal__open");  // Hide the modal
+        });
+
+        // Optional: Close the modal if the user clicks outside the modal
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.classList.remove("form-modal__open");  // Hide the modal
+            }
         });
     };
+}
 
-    // Open the form modal
-    static openForm(form, id) {
-        const modal = document.querySelector(`.form-modal[data-id="${id}"]`);
-        if (modal) {
-            modal.classList.add("form-modal__open");
-        }
-    }
+// Initialize the modal opening and closing functionality
+Ui.openFormOnClick();
 
-    // Close the form modal
-    static closeForm(form, id) {
-        const modal = document.querySelector(`.form-modal[data-id="${id}"]`);
-        if (modal) {
-            modal.classList.remove("form-modal__open");
-        }
-    }
-};
 
 export default Ui;
--- OO-P/src/js/app.js
+++ OO-P/src/js/app.js
@@ -1,4 +1,4 @@
 import Ui from './Ui.js'; // Import the Ui class
 
 // Example usage of the class
-Ui.openFormOnClick('.form-modal', 'all');  // This assumes you want to open the modal with the id 'all'
+Ui.openFormOnClick('.form-modal__all', 'all');  // This assumes you want to open the modal with the id 'all'
